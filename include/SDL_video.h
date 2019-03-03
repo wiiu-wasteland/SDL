f814e40fcfc8b5deb92bccc27036390ae82a465b
Kuruyia <Arc13@users.noreply.github.com>
2019-03-03 13:19:55 +0100

    video/wiiu: Add flags to only draw on TV or Gamepad (#1)

--- include/SDL_video.h
+++ include/SDL_video.h
@@ -113,13 +113,15 @@ typedef enum
     SDL_WINDOW_ALLOW_HIGHDPI = 0x00002000,      /**< window should be created in high-DPI mode if supported.
                                                      On macOS NSHighResolutionCapable must be set true in the
                                                      application's Info.plist for this to have any effect. */
-    SDL_WINDOW_MOUSE_CAPTURE = 0x00004000,      /**< window has mouse captured (unrelated to INPUT_GRABBED) */
-    SDL_WINDOW_ALWAYS_ON_TOP = 0x00008000,      /**< window should always be above others */
-    SDL_WINDOW_SKIP_TASKBAR  = 0x00010000,      /**< window should not be added to the taskbar */
-    SDL_WINDOW_UTILITY       = 0x00020000,      /**< window should be treated as a utility window */
-    SDL_WINDOW_TOOLTIP       = 0x00040000,      /**< window should be treated as a tooltip */
-    SDL_WINDOW_POPUP_MENU    = 0x00080000,      /**< window should be treated as a popup menu */
-    SDL_WINDOW_VULKAN        = 0x10000000       /**< window usable for Vulkan surface */
+    SDL_WINDOW_MOUSE_CAPTURE     = 0x00004000,      /**< window has mouse captured (unrelated to INPUT_GRABBED) */
+    SDL_WINDOW_ALWAYS_ON_TOP     = 0x00008000,      /**< window should always be above others */
+    SDL_WINDOW_SKIP_TASKBAR      = 0x00010000,      /**< window should not be added to the taskbar */
+    SDL_WINDOW_UTILITY           = 0x00020000,      /**< window should be treated as a utility window */
+    SDL_WINDOW_TOOLTIP           = 0x00040000,      /**< window should be treated as a tooltip */
+    SDL_WINDOW_POPUP_MENU        = 0x00080000,      /**< window should be treated as a popup menu */
+    SDL_WINDOW_WIIU_GAMEPAD_ONLY = 0x01000000,      /**< Wii U: window must be drawn only on the Gamepad */
+    SDL_WINDOW_WIIU_TV_ONLY      = 0x02000000,      /**< Wii U: window must be drawn only on the TV */
+    SDL_WINDOW_VULKAN            = 0x10000000       /**< window usable for Vulkan surface */
 } SDL_WindowFlags;
 
 /**
